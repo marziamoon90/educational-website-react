a1d5c06efbc4d39614714fc8047833d55bb2c040
Marzia Moon <marzia.moon9900@gmail.com>
2021-10-04 18:45:29 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Classes from './components/Classes/Classes'
+import Services from './components/Services/Services';
+import About from './components/About/About';
+import Notfound from './components/NotFound/Notfound';
+import JoinClass from './components/JoinClass/JoinClass';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div style={{ minHeight: '95vh' }}>
+          <Header></Header>
+          <Switch>
+            <Route path='/home'>
+              <Home></Home>
+            </Route>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
+            <Route path="/classes">
+              <Classes></Classes>
+            </Route>
+            <Route exact path="/class/:classId">
+              <JoinClass></JoinClass>
+            </Route>
+            <Route path='/services'>
+              <Services></Services>
+            </Route>
+            <Route path='/about'>
+              <About></About>
+            </Route>
+            <Route path='*'>
+              <Notfound></Notfound>
+            </Route>
+          </Switch>
+        </div>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-dark text-muted p-3 text-center'>
+            <h4 className="text-light ">All Your Color Is Here,Color Your Steps</h4>
+            <small>&copy; copyright by color your steps 2021</small>
+        </div>
+    );
+};
+
+export default Footer;
